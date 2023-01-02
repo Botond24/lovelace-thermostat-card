24a1b8dd75d070864c850cf5115356b8e588eb88
Botond24 <44529287+Botond24@users.noreply.github.com>
2023-01-03 00:46:16 +0100

    Update thermostat-card-test.js

--- dist/thermostat-card-test.js
+++ dist/thermostat-card-test.js
@@ -1,5 +1,5 @@
-import {cssData} from './styles.js?v=1.3.0';
-import ThermostatUI from './thermostat_card.lib.js?v=1.3.0';
+import {cssData} from './styles.js';
+import ThermostatUI from './thermostat_card.lib.js';
 console.info("%c Thermostat Card \n%c  Version  1.3.0 ", "color: orange; font-weight: bold; background: black", "color: white; font-weight: bold; background: dimgray");
 class ThermostatCard extends HTMLElement {
   constructor() {
